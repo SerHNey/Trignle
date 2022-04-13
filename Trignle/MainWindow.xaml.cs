050168b9deb555370c92def6ad51a314095078ce
SerHNow <naumov.sergey2003@gmail.com>
2022-04-13 23:07:16 +0300

    Добавьте файлы проекта.

---
+++ Trignle/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Trignle
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            num1.Text = "Введите первую сторону треугольника";
+            num2.Text = "Введите вторую сторону треугольника";
+            num3.Text = "Введите третью сторону треугольника";
+        }
+
+        private void Vvod_Click(object sender, RoutedEventArgs e)
+        {
+            MethodTrigl trigl = new MethodTrigl();
+
+            int number1 = trigl.Proverka(num1.Text);
+            int number2 = trigl.Proverka(num2.Text);
+            int number3 = trigl.Proverka(num3.Text);
+            if (number1 != 0 && number2 != 0 && number3 != 0)
+            {
+                  trigl.Treug(number1, number2, number3);
+                  OutResult.Text = trigl.Treug(number1, number2, number3);
+            }
+            else
+            {
+                OutResult.Text = "Введены некорректные данные";
+            }
+        }
+
+        private void num1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            num1.Clear();
+        }
+
+        private void num2_GotFocus(object sender, RoutedEventArgs e)
+        {
+            num2.Clear();
+        }
+
+        private void num3_GotFocus(object sender, RoutedEventArgs e)
+        {
+            num3.Clear();
+        }
+    }
+}
---
+++ Trignle/MethodTrigl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trignle
+{
+    internal class MethodTrigl
+    {
+        public int a, b, c;
+        public int Proverka(string numb)
+        {
+            try
+            {
+                int num = Convert.ToInt32(numb);
+                if (num != 0 && num > 0)
+                {
+                    return num;
+                }else 
+                    return 0;
+
+            }
+            catch (FormatException) { return 0; }
+
+        }
+        public string Treug(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a)
+            {
+                return "Треугольника с такими сторонами не существует!!!";
+            }
+            if (a == b && a == c && b == c)
+            {
+                return "Треугольник равносторонний!";
+            }
+            if (((a * a) == (b * b) + (c * c)) || ((b * b) == ((a * a) + (c * c)) || ((c * c) == (b * b) + (a * a))))
+            {
+                return "Треугольник прямоугольный!";
+            }
+            if ((a == b && a != c) || (b == c && b != a) || (a == c && a != b))
+            {
+                return "Треугольник равнобедренный!";
+            }
+            return "Треугольник разносторонний!";
+        }
+    }
+}
